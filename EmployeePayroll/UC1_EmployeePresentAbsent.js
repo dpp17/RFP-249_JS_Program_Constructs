f8664ef4906b40d8bb0bc642d81ac078b90fd711
dpp17 <durga123q.com>
2023-03-14 12:16:46 +0530

    Added:EmployeePayroll Programming construct

---
+++ EmployeePayroll/UC1_EmployeePresentAbsent.js
@@ -0,0 +1,9 @@
+const is_Absent = 0;
+
+let check = Math.floor(Math.random()*10)%2;
+if(check == is_Absent){
+    console.log(" Employee is Absent..");
+    return;
+}else{
+    console.log(" Employee is Present..")
+}
